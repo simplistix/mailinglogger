d69f8e54f81a5d38696020fa87ea605b97806650
chris <chris@ac150577-6901-3246-a117-cd8547e6756f>
2007-05-09 07:39:10 +0000

    factor out description text to allow for easy rst2html'ing

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@
 # http://www.opensource.org/licenses/mit-license.html
 # See license.txt for more details.
 
+import os
 from setuptools import setup, find_packages
 
 setup(
@@ -13,29 +14,7 @@ setup(
     author_email='chris@simplistix.co.uk',
     license='MIT',
     description="Enhanced emailing handlers for the python logging package.",
-    long_description="""
-    This package contains two handlers for the python logging
-    framework that enable important log entries to be sent by email.
-
-    This can either be as the entries are logged or as a summary at
-    the end of the running process.
-
-    The handlers have the following features:
-    
-    - customisable and dynamic subject lines for emails sent
-
-    - emails sent with an X-Mailer header for easy filtering
-    
-    - flood protection to ensure the number of emails sent is not
-      excessive
-
-    - fully documented and tested
-    
-    In addition, extra support is provided for configuring the
-    handlers when using ZConfig, Zope 2 or Zope 3.
-
-    For full documentation, start with the readme.txt in the source distribution.
-    """,
+    long_description=open(os.path.join('docs','description.txt')).read(),
     url='http://www.simplistix.co.uk/software/python/mailinglogger',
     keywords="logging email",
     classifiers=[
