86fbf00559789815453b853eff1c5f84b0ae706d
Gil Forcada <gforcada@gnome.org>
2016-04-05 23:16:28 +0200

    autopep8

--- mailinglogger/tests/shared.py
+++ mailinglogger/tests/shared.py
@@ -5,83 +5,86 @@ import atexit
 import logging
 import smtplib
 
+
 class DummySMTP:
 
     broken = False
 
     sent = []
 
-    stdout=True
-    
+    stdout = True
+
     old_smtp = None
 
     username = None
     password = None
-    
+
     @staticmethod
     def install(stdout=True):
         if DummySMTP.old_smtp is None:
             DummySMTP.old_smtp = smtplib.SMTP
             smtplib.SMTP = DummySMTP
         DummySMTP.sent = []
-        DummySMTP.stdout=stdout
+        DummySMTP.stdout = stdout
 
     @staticmethod
     def remove():
         if DummySMTP.old_smtp is not None:
             smtplib.SMTP = DummySMTP.old_smtp
             DummySMTP.old_smtp = None
-        DummySMTP.stdout=True
-    
+        DummySMTP.stdout = True
+
     def __init__(self, mailhost, port=25):
         if self.broken:
             raise RuntimeError
         self.mailhost = mailhost
         self.port = port
-        
-    def login(self,username,password):
+
+    def login(self, username, password):
         self.username = username
         self.password = password
-    
-    def sendmail(self,fromaddr,toaddrs,msg):
-        msg = msg.replace('\r\n','\n')
+
+    def sendmail(self, fromaddr, toaddrs, msg):
+        msg = msg.replace('\r\n', '\n')
         if self.stdout:
             print 'sending to %r from %r using %r' % (
-                toaddrs,fromaddr,(self.mailhost,self.port)
-                )
+                toaddrs, fromaddr, (self.mailhost, self.port)
+            )
             if self.username and self.password:
                 print '(authenticated using username:%r and password:%r)' % (
                     self.username,
                     self.password,
-                    )
+                )
             print msg
         else:
             self.sent.append((
                 toaddrs,
                 fromaddr,
-                (self.mailhost,self.port),
+                (self.mailhost, self.port),
                 msg,
                 self.username,
                 self.password
-                ))
-        
+            ))
+
     def quit(self):
         pass
 
+
 class Dummy:
 
-    def __init__(self,value):
+    def __init__(self, value):
         self.value = value
 
     def __call__(self, *args):
         return self.value
-    
+
+
 def removeHandlers():
     to_handle = [logging.getLogger()]
     for logger in logging.Logger.manager.loggerDict.values():
         to_handle.append(logger)
     for logger in to_handle:
-        if isinstance(logger,logging.PlaceHolder):
+        if isinstance(logger, logging.PlaceHolder):
             continue
         for handler in list(logger.handlers):
             logger.removeHandler(handler)
@@ -89,19 +92,20 @@ def removeHandlers():
     for k in tuple(logging._handlers.keys()):
         del logging._handlers[k]
     # the new way
-    hl = getattr(logging,'_handlerList',None)
+    hl = getattr(logging, '_handlerList', None)
     if hl:
-        hl[:]=[]
-    
+        hl[:] = []
+
+
 def _setUp(test, self=None, stdout=True):
     if self is None:
         d = test.globs
     else:
         d = self.__dict__
-        
+
     removeHandlers()
     DummySMTP.install(stdout=stdout)
-    
+
     datetime = test_datetime(2007, 1, 1, 10, delta=0)
     time = test_time(2007, 1, 1, 10, delta=0)
     r = Replacer()
@@ -112,11 +116,12 @@ def _setUp(test, self=None, stdout=True):
     tzset()
 
     d['r'] = r
-    d['smtp']=DummySMTP
-    d['datetime']=datetime
-    d['time']=time
-    d['removeHandlers']=removeHandlers
-    
+    d['smtp'] = DummySMTP
+    d['datetime'] = datetime
+    d['time'] = time
+    d['removeHandlers'] = removeHandlers
+
+
 def _tearDown(test, self=None):
     if self is None:
         d = test.globs
@@ -124,9 +129,8 @@ def _tearDown(test, self=None):
         d = self.__dict__
     # restore stuff we've mocked out
     d['r'].restore()
-    tzset()    
+    tzset()
     # make sure we have no dummy smtp
     DummySMTP.remove()
     # make sure we haven't registered any atexit funcs
     atexit._exithandlers[:] = []
-    
--- mailinglogger/tests/test_docs.py
+++ mailinglogger/tests/test_docs.py
@@ -5,17 +5,19 @@
 # See license.txt for more details.
 
 import unittest
-from doctest import REPORT_NDIFF,ELLIPSIS
+from doctest import REPORT_NDIFF, ELLIPSIS
 from manuel import doctest, codeblock
 from manuel.testing import TestSuite
-from os.path import dirname,pardir,join
+from os.path import dirname, pardir, join
 from shared import _setUp, _tearDown
 
 docs_dir = join(dirname(__file__), pardir, pardir, 'docs')
 
-options = REPORT_NDIFF|ELLIPSIS
+options = REPORT_NDIFF | ELLIPSIS
+
+
 def test_suite():
-    m =  doctest.Manuel(optionflags=REPORT_NDIFF|ELLIPSIS)
+    m = doctest.Manuel(optionflags=options)
     m += codeblock.Manuel()
     return TestSuite(
         m,
@@ -23,9 +25,9 @@ def test_suite():
         join(docs_dir, 'summarisinglogger.txt'),
         join(docs_dir, 'subjectformatter.txt'),
         join(docs_dir, 'html.txt'),
-        setUp = _setUp,
-        tearDown = _tearDown,
-        )
+        setUp=_setUp,
+        tearDown=_tearDown,
+    )
 
 if __name__ == '__main__':
     unittest.main(default='test_suite')
